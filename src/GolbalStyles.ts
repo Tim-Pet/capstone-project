f7cdafc96696856366e47b2d2486b12405024770
Tim Petersen <tim.petersen1@outlook.de>
2021-06-07 11:06:00 +0200

    Add Global Styles & Montserrat-font

--- src/GolbalStyles.ts
+++ src/GolbalStyles.ts
@@ -1,13 +1,31 @@
 import { createGlobalStyle } from 'styled-components'
+import './assets/fonts/Montserrat/Montserrat-Regular.ttf'
+import './assets/fonts/Montserrat/Montserrat-Thin.ttf'
 
 const GlobalStyle = createGlobalStyle`
+@font-face {
+  font-family: "Montserrat";
+  src: local("Montserrat"),
+  url("./assets/fonts/Montserrat/Montserrat-Regular.ttf") format("truetype");
+}
+
+:root {
+  --color-accent: #007BF5;
+  --color-accent-light: #007bf570;
+  --color-background: linear-gradient(to right top, #ffffff, #fbfbfb, #f7f7f7, #f3f3f3, #efefef);
+  --color-background-alt: #FFFFFF70;
+  --color-text: #505254;
+  --color-text-light: #50525470;
+}
+
 * {
   box-sizing: border-box;
 }
 
 body {
-  color: #333;
-  font-family: sans-serif;
+  color: var(--color-text);
+  font-family: Montserrat, sans-serif;
+  font-weight: 400;
   margin: 0;
   padding: 0;
 }
