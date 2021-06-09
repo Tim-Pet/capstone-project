7505c363455e2c6da93a0eac18ba5534a6557a56
Tim Petersen <tim.petersen1@outlook.de>
2021-06-09 17:44:41 +0200

    Create jest mock for 3rd party lib & Slider test

---
+++ src/components/common/Slider/Slider.spec.tsx
@@ -0,0 +1,50 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import Slider from './Slider'
+
+jest.mock('@material-ui/core/Slider', () => (props: any) => {
+  const { startValue, min, max, step, onChange } = props
+  return (
+    <input
+      type="range"
+      defaultValue={startValue}
+      min={min}
+      max={max}
+      step={step}
+      onChange={event => onChange(event.target.value)}
+    />
+  )
+})
+
+describe('Slider', (): void => {
+  it('Slider gets rendered', (): void => {
+    render(
+      <Slider
+        title={'testSlider'}
+        startValue={0}
+        min={0}
+        max={1}
+        step={0.01}
+        onChange={jest.fn()}
+      />
+    )
+    const slider: HTMLElement = screen.getByRole('slider')
+    expect(slider).toBeInTheDocument()
+  })
+  it('Slider calls onChange', (): void => {
+    const handleChange: Function = jest.fn()
+    render(
+      <Slider
+        data-testid={'test'}
+        title={'testSlider'}
+        startValue={0.3}
+        min={0}
+        max={1}
+        step={0.01}
+        onChange={handleChange}
+      />
+    )
+    const slider: HTMLElement = screen.getByRole('slider')
+    fireEvent.change(slider, 0.1)
+    expect(handleChange).toBeCalledTimes(1)
+  })
+})
