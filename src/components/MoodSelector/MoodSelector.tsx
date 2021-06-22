d6a6af92e692743ffa2f7897c69ac6ba44b2d884
Tim Petersen <tim.petersen1@outlook.de>
2021-06-22 11:55:21 +0200

    Add Styling to match general design

--- src/components/MoodSelector/MoodSelector.tsx
+++ src/components/MoodSelector/MoodSelector.tsx
@@ -18,7 +18,7 @@ interface Variables {
   topLine: number
 }
 
-const MoodSelector = ({ moods, preselectedMoodId }: Props) => {
+function MoodSelector({ moods, preselectedMoodId }: Props): JSX.Element {
   const [currentMood, setCurrentMood] = useState(0) // will get taken out to higher order component
 
   const moodListRef = useRef<HTMLUListElement>(null)
@@ -29,8 +29,8 @@ const MoodSelector = ({ moods, preselectedMoodId }: Props) => {
       const moodItems = [...moodListRef.current.children] as Array<HTMLElement>
       const moodItemCenter = moodItems[0].offsetHeight / 2
       const boxCenter = moodListRef.current.offsetHeight / 2
-      const bottomLine = boxCenter + 11 // 11px below center
-      const topLine = boxCenter - 21 // 21px above center --> 32px(2rem) in total
+      const bottomLine = boxCenter + 15 // 11px below center
+      const topLine = boxCenter - 25 // 21px above center --> 40px(2.5rem) in total
 
       variables.current = {
         moodList: moodListRef.current,
@@ -47,16 +47,18 @@ const MoodSelector = ({ moods, preselectedMoodId }: Props) => {
   let activeMood: number
 
   return (
-    <TmpContainer>
-      <InnerContainer onScroll={handleScroll} ref={moodListRef}>
-        {moods.map(({ name, id }) => (
-          <MoodItem key={id} data-id={id}>
-            {name}
-          </MoodItem>
-        ))}
-      </InnerContainer>
-      <Button onClick={handleClick}>Done</Button>
-    </TmpContainer>
+    <Wrapper>
+      <OuterContainer>
+        <InnerContainer onScroll={handleScroll} ref={moodListRef}>
+          {moods.map(({ name, id }) => (
+            <MoodItem key={id} data-id={id}>
+              {name}
+            </MoodItem>
+          ))}
+        </InnerContainer>
+        <Button onClick={handleClick}>Done</Button>
+      </OuterContainer>
+    </Wrapper>
   )
 
   function setInitialMood(initialMoodId: number) {
@@ -107,20 +109,38 @@ const MoodSelector = ({ moods, preselectedMoodId }: Props) => {
 }
 
 export default MoodSelector
-const TmpContainer = styled(Div100vh)`
-  width: 100vw;
+const Wrapper = styled(Div100vh)`
+  position: fixed;
   display: grid;
   place-items: center;
+  width: 100vw;
+  padding: 0 15px;
+  background-color: transparent;
+`
+const OuterContainer = styled.div`
+  height: 350px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  border-radius: 36px;
+  box-shadow: -2px 2px 16px var(--color-text-light);
 `
 const InnerContainer = styled.ul`
-  height: 100px;
+  height: 150px;
   width: 300px;
-  border: 1px solid black;
+  border: 1px solid var(--color-text-light);
+  border-radius: 24px;
   overflow-y: scroll;
   overflow-x: hidden;
   -webkit-overflow-scrolling: touch;
+  scrollbar-width: none;
   list-style: none;
   text-align: center;
-  padding: 2rem 0;
-  line-height: 2rem;
+  padding: 2.5rem 0;
+  line-height: 2.5rem;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `
