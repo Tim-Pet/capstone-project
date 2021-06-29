b3ebb044b0f99ee751039c048f34b90e03686b13
Tim Petersen <tim.petersen1@outlook.de>
2021-06-29 13:41:31 +0200

    Add axios functions

---
+++ client/src/helper/backendRequests.ts
@@ -0,0 +1,28 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:4000'
+
+export async function getUser(id: string) {
+  return await axios.get(`${baseUrl}/api/users/${id}`)
+}
+
+//inhalte mitgeben
+export async function createUser(id: string, name: string) {
+  return await axios({
+    method: 'post',
+    url: `${baseUrl}/api/users`,
+    headers: {},
+    data: {
+      _id: id,
+      name: name,
+    },
+  })
+}
+
+export async function updateUser(id: string, playlists: any[]) {
+  return await axios({
+    method: 'patch',
+    url: `${baseUrl}/api/users/${id}`,
+    headers: {},
+    data: { playlists: playlists },
+  })
+}
